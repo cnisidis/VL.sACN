60e6fb07369cda6590ea79f56f3a1fae6ae5ea1c
cnisidis <makeithappen@nisidis.com>
2025-08-06 20:10:12 +0300

    breaking Changes , custom made solution to Deserialize sacn packets

---
+++ csharp/sACN/Structs.cs
@@ -0,0 +1,174 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sACN
+{
+    public enum FramingOptionsType
+    {
+        E131_OPT_TERMINATED = 6,
+        E131_OPT_PREVIEW = 7,
+
+    }
+
+    public enum ErrorType
+    {
+        E131_ERR_NONE,
+        E131_ERR_NULLPTR,
+        E131_ERR_PREAMBLE_SIZE,
+        E131_ERR_POSTAMBLE_SIZE,
+        E131_ERR_ACN_PID,
+        E131_ERR_VECTOR_ROOT,
+        E131_ERR_VECTOR_FRAME,
+        E131_ERR_VECTOR_DMP,
+        E131_ERR_TYPE_DMP,
+        E131_ERR_FIRST_ADDR_DMP,
+        E131_ERR_ADDR_INC_DMP,
+
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1, CharSet = CharSet.Ansi)]
+    public struct ARootLayer
+    {
+        public ushort preamble_size;     // Preamble Size (uint16_t)
+        public ushort postamble_size;    // Post-amble Size (uint16_t)
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 12)]
+        public byte[] acn_pid;           // ACN Packet Identifier (uint8_t[12])
+        public ushort flength;           // Flags (high 4 bits) & Length (low 12 bits) (uint16_t)
+        public uint vector;              // Layer Vector (uint32_t)
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 16)]
+        public byte[] cid;               // Component Identifier (UUID) (uint8_t[16])
+
+
+
+        public Guid GetCID()
+        {
+            return new Guid(cid);   
+        }
+
+        public string GetIdentifier()
+        {
+            return Encoding.UTF8.GetString(acn_pid).Replace('\0',' ').TrimEnd();
+            
+        }
+
+        public void Split(out string Identifier, out Guid CID)
+        {
+            Identifier = GetIdentifier();
+            CID = GetCID();
+        }
+        public void Info(out int length, out byte flags)
+        {
+            flags = (byte)(flength >> 12);
+
+            // Extract low 12 bits for length
+            // Use a bitwise AND with a mask (0x0FFF) to isolate the low 12 bits.
+            length = (ushort)(flength & 0x0FFF);
+
+
+        }
+    }
+    [StructLayout(LayoutKind.Sequential, Pack = 1, CharSet = CharSet.Ansi)]
+    public struct AFramingLayer
+    {
+        public ushort flength;           // Flags (high 4 bits) & Length (low 12 bits) (uint16_t)
+        public uint vector;              // Layer Vector (uint32_t)
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 64)]
+        public byte[] source_name;       // User Assigned Name of Source (UTF-8) (uint8_t[64])
+        public byte priority;            // Packet Priority (0-200, default 100) (uint8_t)
+        public ushort reserved;          // Reserved (should be always 0) (uint16_t)
+        public byte seq_number;          // Sequence Number (uint8_t)
+        public byte options;             // Options Flags (uint8_t)
+        public ushort universe;          // DMX Universe Number (uint16_t)
+
+        public string GetName()
+        {
+            return Encoding.UTF8.GetString(source_name).Replace('\0', ' ').TrimEnd();
+        }
+
+        public int GetSeqNumber()
+        {
+            return (int)seq_number;
+        }
+
+        public int GetUniverse()
+        {
+            return universe;
+        }
+
+        public void  Info(out int length, out byte flags )
+        {
+            flags = (byte)(flength >> 12);
+
+            // Extract low 12 bits for length
+            // Use a bitwise AND with a mask (0x0FFF) to isolate the low 12 bits.
+            length = (ushort)(flength & 0x0FFF);
+
+
+        }
+
+        public void Split(out string Name, out int Universe, out int SeqNumber, out FramingOptionsType Type, out int Priority)
+        {
+            Name = GetName();
+            Universe = (int)this.universe;
+            SeqNumber = (int)this.seq_number;   
+            Type = (FramingOptionsType)options;
+            Priority = (int)this.priority;
+        }
+
+    }
+
+    [StructLayout(LayoutKind.Sequential, Pack = 1, CharSet = CharSet.Ansi)]
+    public struct ADMPLayer
+    {
+        ushort flength;          /* Flags (high 4 bits) / Length (low 12 bits) */
+        byte vector;           /* Layer Vector */
+        ushort type;             /* Address Type & Data Type */
+        ushort first_addr;       /* First Property Address */
+        ushort addr_inc;         /* Address Increment */
+        ushort prop_val_cnt;     /* Property Value Count (1 + number of slots) */
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 513)]
+        byte[] prop_val;    /* Property Values (DMX start code + slots data) */
+
+
+        public Spread<byte> GetDMXValues()
+        {
+            return prop_val.Take(512).ToSpread();
+        }
+        
+        public int GetValuesCount()
+        {
+            return prop_val_cnt;
+        }
+
+        public int GetStartCode()
+        {
+            return (int)prop_val[512];
+        }
+
+        public void Split(out int Values, out int StartCode)
+        {
+            Values = prop_val_cnt;
+            StartCode = GetStartCode();
+        }
+
+        public void Info(out int length, out byte flags)
+        {
+            flags = (byte)(flength >> 12);
+
+            // Extract low 12 bits for length
+            // Use a bitwise AND with a mask (0x0FFF) to isolate the low 12 bits.
+            length = (ushort)(flength & 0x0FFF);
+
+
+        }
+    }
+
+    
+}
+
---
+++ csharp/sACN/Utils.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Markup;
+
+namespace sACN
+{
+    public static class Utils
+    {
+        public const int PORT = 5568;
+        public static T Deserialize<T>(byte[] data) where T : struct
+        {
+            // Allocate a GCHandle to "pin" the byte array in memory,
+            // preventing the garbage collector from moving it.
+            GCHandle handle = GCHandle.Alloc(data, GCHandleType.Pinned);
+            try
+            {
+                // Get the address of the pinned data.
+                IntPtr ptr = handle.AddrOfPinnedObject();
+
+                // Use Marshal.PtrToStructure to copy the data from the unmanaged memory
+                // location to a new instance of the specified struct type.
+                // The method automatically handles the layout based on the StructLayout attribute.
+                return (T)Marshal.PtrToStructure(ptr, typeof(T));
+            }
+            finally
+            {
+                // Always free the GCHandle to avoid memory leaks.
+                if (handle.IsAllocated)
+                {
+                    handle.Free();
+                }
+            }
+        }
+
+        // You can also create a specific function for your RootLayer.
+        public static ARootLayer DeserializeRootLayer(byte[] data)
+        {
+            return Deserialize<ARootLayer>(data);
+        }
+
+        public static AFramingLayer DeserializeFramingLayer(byte[] data)
+        {
+            return Deserialize<AFramingLayer>(data);
+        }
+
+        public static ADMPLayer DeserializeDMPLayer(byte[] data)
+        {
+            return Deserialize<ADMPLayer>(data);
+        }
+
+        public static APacket DeserializeAPacket(byte[] data)
+        {
+            var packet = new APacket();
+            packet.RootLayer = Deserialize<ARootLayer>(data);
+            packet.FramingLayer = Deserialize<AFramingLayer>(data.Skip(38).ToArray());
+            packet.DMPLayer = Deserialize<ADMPLayer>(data.Skip(38+77).ToArray());
+            return packet;
+        }
+
+
+        public static ushort CombineFlagsAndLength(byte flags, ushort length)
+        {
+            if (flags > 0xF) // 4 bits can hold values 0-15 (0x0 to 0xF)
+            {
+                throw new ArgumentOutOfRangeException(nameof(flags), "Flags value must be between 0 and 15.");
+            }
+            if (length > 0xFFF) // 12 bits can hold values 0-4095 (0x0 to 0xFFF)
+            {
+                throw new ArgumentOutOfRangeException(nameof(length), "Length value must be between 0 and 4095.");
+            }
+
+            // Shift flags to the high 4 bits position
+            ushort combinedFlength = (ushort)(flags << 12);
+
+            // Combine with length using bitwise OR
+            combinedFlength |= length;
+
+            return combinedFlength;
+        }
+    }
+
+    public class APacket
+    {
+        public ARootLayer RootLayer;
+        public AFramingLayer FramingLayer;
+        public ADMPLayer DMPLayer;
+
+        private string Identifier = "ASC - E1.17";
+        private byte[] Values;
+        private int Priority;
+        Guid CID;
+        
+        private int Sequence;
+        private string Name;
+
+        public APacket() { }
+        public APacket(string Name, int Priority = 100)
+        {
+            this.RootLayer = new ARootLayer();
+            this.FramingLayer = new AFramingLayer();
+            this.DMPLayer = new ADMPLayer();
+            this.CID = new Guid();
+            this.Priority = Priority;
+            this.Name = Name;
+        }
+
+        public void Split(out ARootLayer RootLayer, out AFramingLayer FramingLayer, out ADMPLayer DMPLayer)
+        {
+            RootLayer = this.RootLayer;
+            FramingLayer = this.FramingLayer;
+            DMPLayer = this.DMPLayer;
+        }
+
+
+        public void SetValues(Spread<byte> Values)
+        {
+            this.Values = Values.ToArray();
+        }
+        
+        public void SetPriority(int Value)
+        {
+            this.Priority = Value;
+        }
+
+        public void SetName(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public void Update()
+        {
+            Sequence ++;
+            Sequence = Sequence % 256;
+        }
+
+        public void GetSequence(out int Seq)
+        {
+            Seq = this.Sequence;
+        }
+        
+    }
+
+
+
+}
+
